e48e99234f406e6aa7bc09b87227edb109a84892
steventruong1 <97932227+steventruong1@users.noreply.github.com>
2023-11-16 23:38:34 -0500

    Update portScanner.py

--- portScanner.py
+++ portScanner.py
@@ -4,23 +4,23 @@ import ipaddress
 import concurrent.futures
 from threading import Lock
 
-LOCK = Lock()
+LOCK = Lock() #mutex
 
 def doprint(msg):
-    with LOCK:
+    with LOCK: #Threads will grab mutex and print message.
         print(msg)
 
 def scan(portNumber):
     s = socket(AF_INET, SOCK_STREAM)
     try:
-        con = s.connect((address,portNumber))
+        con = s.connect((address,portNumber)) #Attempt to connect via socket. If we can, that means it's open
         doprint(f"Port {portNumber} is open")
         return True
     except:
         return False
 def ip_validator(ip):
     try:
-        ip_obj = ipaddress.ip_address(ip)
+        ip_obj = ipaddress.ip_address(ip) 
         print(f"{ip} is a valid IP address")
         return ip
     except:
@@ -36,15 +36,14 @@ def portNumber_validator(portNumber):
             exit()
     return portNumber
     
-address = ip_validator(gethostbyname(input('Enter address: ')))
-portNumber = portNumber_validator(int(input('Enter Upper Range Port Number to Check?: ')))
+address = ip_validator(gethostbyname(input('Enter address: '))) #grabs input and validates it is a valid address
+portNumber = portNumber_validator(int(input('Enter Upper Range Port Number to Check?: '))) #iterates from 1 to number for open ports
 
 start_time = time.time()
 print("Starting Port Scanning ...")
 futures=[]
-with concurrent.futures.ThreadPoolExecutor(max_workers=portNumber) as executor:
-     future = executor.map(scan,[*range(1,portNumber+1)])
-for future in futures:
-    print(future)
+with concurrent.futures.ThreadPoolExecutor(max_workers=portNumber) as executor: #Thread pools
+     executor.map(scan,[*range(1,portNumber+1)])
+
 
 print("Port Scanning Complete",time.time() - start_time,"seconds to run")
